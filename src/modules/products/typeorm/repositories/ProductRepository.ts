64a6b2a595f2d3f19f2303afa3cb835e834acb9f
Daniel <dfpires@gmail.com>
2021-08-16 20:11:09 -0300

    versão 1.0

---
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -0,0 +1,18 @@
+
+import {Repository} from 'typeorm'
+import Product from '../entities/Product'
+export default class ProductRepository extends Repository<Product> {
+
+    // exemplo de uma implementação particular, pois é resto já temos
+    public async findByName(name: string): Promise<Product | undefined> {
+        let product = this.findOne({ // procure o primeiro nome encontrado
+            where: {
+                name
+            }
+        })
+
+        return product;
+    }
+
+}
+
