23bd9473a8ec03bcbe780dd2465800f86972ca0c
Daniel Pires <dfpires@gmail.com>
2021-09-13 20:01:19 -0300

    13-09

--- src/modules/products/routes/product.routes.ts
+++ src/modules/products/routes/product.routes.ts
@@ -1,5 +1,7 @@
 
+import { celebrate, Segments, Joi } from 'celebrate';
 import {Router} from 'express';
+
 
 import ProductController from '../controllers/ProductController';
 
@@ -7,9 +9,19 @@ let productRouter = Router()
 
 let productController = new ProductController()
 
+// não temos nada para validar
 productRouter.get('/', productController.index)
 productRouter.get('/:id', productController.show)
-productRouter.post('/', productController.create)
+// validar que o post precisa de um produto
+productRouter.post('/', 
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required()
+        }
+    }),
+productController.create)
 productRouter.put('/:id', productController.update)
 productRouter.delete('/:id', productController.delete)
 
