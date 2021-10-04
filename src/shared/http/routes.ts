8bfee7040f7a9e76aebe80f40af350d7991dff62
Daniel Pires <dfpires@gmail.com>
2021-10-04 19:50:47 -0300

    04-10

--- src/shared/http/routes.ts
+++ src/shared/http/routes.ts
@@ -6,4 +6,5 @@ import productRouter from '../../modules/products/routes/product.routes'
 
 routes.use('/products', productRouter)
 
+// alterar o arquivo para incluir rota de cliente
 export default routes
