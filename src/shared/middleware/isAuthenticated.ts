5340e929708e83d56ec3426a3fa685a96e258280
Daniel Pires <dfpires@gmail.com>
2021-10-18 20:27:07 -0300

    18-10

---
+++ src/shared/middleware/isAuthenticated.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+// verifica se o usuário tem um token válido
+export default function isAuthenticated(request: Request, response: Response, next: NextFunction): void {
+
+    // recupera o cabeçalho da requisição contendo a autorização - token
+    const authHeader = request.headers.authorization
+    if (!authHeader){
+        throw new AppError('Token JWT é inválido')
+    }
+    // Beared fkdsjfklsdjfkljsdklfjlksdjflksdjfkljsdlkfjsldkd
+    const [, token] = authHeader.split(' ') // separa em dois pedaços, e somente o segundo nos interessa
+
+    // verifica se o token é válido
+    try {
+            const tokenVerificado = verify(token, 'dfgsdgdfsgdterwtbcvbgh')
+
+            return next() // pode acessar a API
+    }
+    catch {
+        throw new AppError('Token JWT inválido')
+    }
+}
