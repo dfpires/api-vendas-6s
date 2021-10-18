32cfe5ec40dd85b8c2fcccc2b2f03c4118507e0d
Daniel Pires <dfpires@gmail.com>
2021-10-18 19:45:19 -0300

    18-10

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,50 @@
+// responsável por gerar um token para o usuário
+
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UserRepository from "../typeorm/repository/UserRepository";
+import {compare} from 'bcryptjs'
+import {sign} from 'jsonwebtoken'
+
+// vamos criar um tipo de dado de requisição
+interface IRequest {
+    email: string;
+    password: string
+}
+
+// vamos criar um tipo de dado de resposta
+interface IResponse {
+    user: User;
+    token: string
+}
+
+class CreateSessionService {
+
+    public async execute( {email, password}: IRequest): Promise<IResponse> {
+        // recupera o repositório de User
+        const userRepository = getCustomRepository(UserRepository)
+        const user = await userRepository.findByEmail(email)
+        // verifica se o usuário existe
+        if (!user) {
+            throw new AppError('Email / Senha inválidos', 401);
+        }
+        // verifica se a senha confere
+        const senhaConfirmada = await compare(password, user.password)
+        if (!senhaConfirmada) {
+            throw new AppError('Email / Senha inválidos', 401);
+        }
+
+        // usuário pode receber o token
+        // vamos gerar o toke
+        const token = sign({}, "dfgsdgdfsgdterwtbcvbgh", {
+            subject: user.id,
+            expiresIn: '1d'
+        })
+        // retorna resultado
+        return {
+            user, 
+            token
+        }
+    }
+}
