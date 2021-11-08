8c7df947aded21cfea8b7571973fc790c3efc5a2
Daniel Pires <dfpires@gmail.com>
2021-11-08 19:49:00 -0300

    08-11

--- frontend/gitcollection/src/App.tsx
+++ frontend/gitcollection/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Routes} from './routes/index'
 import {BrowserRouter} from 'react-router-dom'
+import { GlobalStyle } from './styles/global';
 
 // criando componentes funcionais
 const App: React.FC = () => {
   return (
-
-   <BrowserRouter>
-      <Routes/>
-   </BrowserRouter>
-
+    <>
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>
+   <GlobalStyle/>
+    </>
   );
 }
 
---
+++ frontend/gitcollection/src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+import imgBackground from '../assets/background.svg' 
+// criar nossos estilos globais
+export const GlobalStyle = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+      // 16px = 1 rem
+    @media (max-width: 1080px) { // pc
+      font-size: 93.75%; // 15px
+    }
+    @media (max-width: 720px) { // celular
+      font-size: 87.5%; // 14px
+    }
+  }
+  body {
+    background: #f0f0f5 url(${imgBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 2.5rem 1.25rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`
